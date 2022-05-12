9088c3e0012240c74faa0bd805fb91392578f270
Scott Schmalz <scott.schmalz@gmail.com>
2022-05-12 08:50:44 -0400

    adds small comment about some invite logic

--- src/services/invites/handlers/updateInvite.ts
+++ src/services/invites/handlers/updateInvite.ts
@@ -4,7 +4,11 @@ import getInvite from './getInvite';
 import { InviteInput } from 'db';
 
 const updateInvite = async (inviteId: string, { id, affiliateIds, ...data }: InviteInput) => {
-  await validators.validate([validators.inviteCode, validators.organizationId, validators.affiliateIds], { ...data, affiliateIds, id: inviteId });
+  await validators.validate([validators.inviteCode, validators.organizationId, validators.affiliateIds], {
+    ...data,
+    affiliateIds,
+    id: inviteId, // * Adding id in so that the inviteCode validation logic will allow for the duplicate inviteCode
+  });
 
   const invite = await (await getInvite(inviteId)).update(data);
   await invite.$set('affiliates', affiliateIds || null);
